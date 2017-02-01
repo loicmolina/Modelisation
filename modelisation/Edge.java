feebbca0202ed364b8e71c19d28ac60a6fe7ff93
Quentin Morizot <morizot9u@fst-ats-309-l15.fst.site.univ-lorraine.fr>
2017-02-01 11:01:29 +0100

    Version 1.7 et propre

--- modelisation/Edge.java
+++ modelisation/Edge.java
@@ -1,15 +1,12 @@
 package modelisation;
 
-class Edge
-{
+class Edge {
    int from;
    int to;
    int cost;
-   Edge(int x, int y, int cost)
-	 {
-		this.from = x;
-		this.to = y;
-		this.cost = cost;
-	 }
-   
+   Edge(int x, int y, int cost) {
+	   this.from = x;
+	   this.to = y;
+	   this.cost = cost;
+   }
 }
--- modelisation/SeamCarving.java
+++ modelisation/SeamCarving.java
@@ -3,84 +3,80 @@ package modelisation;
 import java.util.ArrayList;
 import java.io.*;
 import java.util.*;
-public class SeamCarving
-{
+public class SeamCarving {
 
-   public static int[][] readpgm(String fn)
-	 {		
-        try {
-            InputStream f = ClassLoader.getSystemClassLoader().getResourceAsStream(fn);
+	public static int[][] readpgm(String fn) {		
+		try {
+			InputStream f = ClassLoader.getSystemClassLoader().getResourceAsStream(fn);
             BufferedReader d = new BufferedReader(new InputStreamReader(f));
             String magic = d.readLine();
             String line = d.readLine();
-		   while (line.startsWith("#")) {
-			  line = d.readLine();
-		   }
-		   Scanner s = new Scanner(line);
-		   int width = s.nextInt();
-		   int height = s.nextInt();		   
-		   line = d.readLine();
-		   s = new Scanner(line);
-		   int maxVal = s.nextInt();
-		   int[][] im = new int[height][width];
-		   s = new Scanner(d);
-		   int count = 0;
-		   while (count < height*width) {
-			  im[count / width][count % width] = s.nextInt();
-			  count++;
-		   }
-		   return im;
-        }
-		
-        catch(Throwable t) {
-            t.printStackTrace(System.err) ;
-            return null;
-        }
-    }
-   
-   public static void writepgm(int[][] image, String filename){
-	   assert(image.length>0):"Tableau vide, impossible de créer l'image :(";	
-	   try {
-		   /*int curseur=0,linesize=0;
-		   if (image.length>30){
+            while (line.startsWith("#")) {
+            	line = d.readLine();
+            }
+            Scanner s = new Scanner(line);
+			int width = s.nextInt();
+			int height = s.nextInt();		   
+			line = d.readLine();
+			s = new Scanner(line);
+			int maxVal = s.nextInt();
+			int[][] im = new int[height][width];
+			s = new Scanner(d);
+			int count = 0;
+			while (count < height*width) {
+				im[count / width][count % width] = s.nextInt();
+				count++;
+			}
+			return im;
+		}
+		catch(Throwable t) {
+			t.printStackTrace(System.err) ;
+			return null;
+		}
+	}
+	
+	public static void writepgm(int[][] image, String filename) {
+		assert(image.length>0):"Tableau vide, impossible de creer l'image :(";	
+		try {
+			/*int curseur=0,linesize=0;
+		   	if (image.length>30){
 			   linesize=30;
-		   }else{
+		   	}else{
 			   linesize=image.length;
-		   }*/
-		   File f=new File(filename+".pgm");
-		   FileWriter fw=new FileWriter(f);
-		   BufferedWriter bw = new BufferedWriter(fw);
-		   bw.write("P2");
-		   bw.newLine();
-		   bw.write(image[0].length+" "+image.length);
-		   bw.newLine();
-		   bw.write("255");
-		   bw.newLine();
+		   	}*/
+			File f=new File(filename+".pgm");
+			FileWriter fw=new FileWriter(f);
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write("P2");
+			bw.newLine();
+			bw.write(image[0].length+" "+image.length);
+			bw.newLine();
+			bw.write("255");
+			bw.newLine();
 
-		   for (int j=0;j<image.length;j++){
-			   for (int i=0;i<image[0].length;i++){
-				   
-				   bw.write(image[j][i]+"\t");
-				   /*curseur++;
-				   if (linesize>0 && curseur==linesize){
+			for (int j=0;j<image.length;j++) {
+				for (int i=0;i<image[0].length;i++) {
+					bw.write(image[j][i]+"\t");
+					/*curseur++;
+				   	if (linesize>0 && curseur==linesize){
 					   bw.newLine();
 					   curseur=0;
-				   }*/
-			   }
-			   bw.newLine();
-			   /*if(linesize==0){
+				   	}*/
+				}
+				bw.newLine();
+				/*if(linesize==0){
 				  
-			   }*/
-		   }
-		   bw.close();
-		   fw.close();
-	   }
-	   catch(Throwable t) {
-           t.printStackTrace(System.err) ;
-       }
-   }
+			   	}*/
+			}
+			bw.close();
+			fw.close();
+		}
+		catch(Throwable t) {
+			t.printStackTrace(System.err);
+		}
+	}
    
-   public static int[][] interest(int[][] image){
+	public static int[][] interest(int[][] image) {
 	   assert(image.length>1):"tableau trop petit";
 	   int[][]tab=new int[image.length][image[0].length];
 	   for (int j=0;j<image.length;j++){
@@ -125,11 +121,9 @@ public class SeamCarving
 	   
    }
    
-   public static void main(String[] args)
-	 {
-		int[][] hello=readpgm("test.pgm");
-		hello=interest(hello);
-		tograph(hello);
-	   	
-	 }
+   public static void main(String[] args) {
+	   int[][] hello=readpgm("test.pgm");
+	   hello=interest(hello);
+	   tograph(hello);
+   }
 }
